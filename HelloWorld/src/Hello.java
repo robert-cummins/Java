7c1d74fb4e04c49160cc3bab99cc7218e14d7000
robert-cummins <robert.k.cummins@gmail.com>
2020-10-07 13:39:01 +1300

    Variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,5 +2,13 @@ public class Hello {
 
     public  static void main(String[] args){
         System.out.println("Hello World");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
     }
 }
